ad5c81ddb99bd27bb052050f364064d65ef1de10
Aleksander Stankowski <s27549@pjwstk.edu.pl>
2025-01-29 22:47:14 +0100

    style: change global font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
+		fontFamily: {
+			sans: ['"poppins"', 'sans-serif']
+		},
   	extend: {
   		borderRadius: {
   			lg: 'var(--radius)',
